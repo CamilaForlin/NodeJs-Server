41b9a1c7fe85a4c0bf40c0ad06975aae9020cbab
Lina616 <camilaforlin616@gmail.com>
2024-11-23 01:59:41 -0300

    conexaoServidor

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbconfig.js";
 
-const posts =[
-    {
-        id: 1,
-        descricao: "um gatinho numa caixa",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 2,
-        descricao: "um cachorro correndo no parque",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 3,
-        descricao: "um peixe nadando no aquário",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 4,
-        descricao: "um pássaro voando no céu",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 5,
-        descricao: "um coelho comendo uma cenoura",
-        imagem: "https://placecats.com/millie/300/150"
-    }
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-]
+async function getTodosPosts() {
+    const db = conexao.db("alura-instabytes")
+    const colecao = db.collection("posts")
+        return colecao.find().toArray()
+    
+};
 
 const app = express();
 app.use(express.json());
@@ -36,7 +17,8 @@ app.listen(3000, () => {
     console.log("servidor escutando");
 });
 
-app.get("/posts",(req,res) => {
+app.get("/posts",async (req,res) => {
+    const posts = await getTodosPosts()
     res.status(200).json(posts);
 });
 
